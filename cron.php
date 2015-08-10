c4c202b05c59e9cee93dd2589b8d9ea99026713c
viper6689 <a349581@drdrb.net>
2015-08-10 18:06:08 +0200

    Create cron.php

---
+++ cron.php
@@ -0,0 +1,5 @@
+<?php
+	file_get_contents('http://lolmeta.netau.net/meta.php');
+	file_get_contents('http://lolmeta.netau.net/summonerInfo.php');
+	file_get_contents('http://lolmeta.netau.net/matchHistory.php');
+?>
