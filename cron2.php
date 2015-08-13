879abc6c58f2e891ace6060c4ef1c6b535c57d41
viper6689 <a349581@drdrb.net>
2015-08-13 13:28:12 +0200

    Update cron2.php

--- cron2.php
+++ cron2.php
@@ -1,5 +1,5 @@
 <?php
 	//every hour
-	file_get_contents('http://lolmeta.serverlux.me//meta.php');
-	file_get_contents('http://lolmeta.serverlux.me//summonerInfo.php');
+	file_get_contents('http://lolmeta.serverlux.me/meta.php');
+	file_get_contents('http://lolmeta.serverlux.me/summonerInfo.php');
 ?>
