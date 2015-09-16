d76a8e78e28110709de93e12914b94bc36444e69
viper6689 <a349581@drdrb.net>
2015-09-16 21:28:16 +0200

    Create teamInfo.php

---
+++ teamInfo.php
@@ -0,0 +1,41 @@
+<?
+	//MySQLi connection
+	$mysqli = new mysqli('localhost', 'rclsirzj_lolmeta', 'LoLMeta42', 'rclsirzj_lolmeta');
+	if($mysqli->connect_errno > 0){
+	   	die('Unable to connect to database [' . $mysqli->connect_error . ']');
+	}
+
+	$teamId = "TEAM-1cc4ac30-3bfe-11e3-b84d-782bcb4ce61a";
+
+	//get JSON teaminfo
+	$json = file_get_contents("https://euw.api.pvp.net/api/lol/euw/v2.5/league/by-team/TEAM-1cc4ac30-3bfe-11e3-b84d-782bcb4ce61a/entry?api_key=92d4e772-44cf-4bc8-9782-236d2c18fcc0");
+	$obj = json_decode($json, true);
+/*
+	$query = '	INSERT INTO teamInfo (
+						teamID, 
+						name,
+						league,
+						division,
+						points)
+					VALUES (
+						"'.$obj[$teamId][0][entries][0][playerOrTeamId].'",  
+						"'.$obj[$teamId][0][entries][0][playerOrTeamName].'", 
+						"'.$obj[$teamId][0][tier].'",
+						"'.$obj[$teamId][0][entries][0][division].'",
+						'.$obj[$teamId][0][entries][0][leaguePoints].')';
+*/
+print_r($obj);
+	$query = '	UPDATE teamInfo
+				SET name 		= "'.$obj[$teamId][0]['entries'][0]['playerOrTeamName'].'",
+					league 		= "'.$obj[$teamId][0]['tier'].'",
+					division	= "'.$obj[$teamId][0]['entries'][0]['division'].'",
+					points		= '.$obj[$teamId][0]['entries'][0]['leaguePoints'].'
+				WHERE teamID = "'.$obj[$teamId][0]['entries'][0]['playerOrTeamId'].'"';
+
+	$mysqli->query($query);
+	unset($query);
+
+
+	//MySQLi disconnection
+	$mysqli->close();
+?>
