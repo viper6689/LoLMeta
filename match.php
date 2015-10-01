70a399228b7d93a964f53f91e066fb93acd0bd1d
viper6689 <a349581@drdrb.net>
2015-10-01 17:19:16 +0200

    Update match.php

--- match.php
+++ match.php
@@ -6,6 +6,14 @@
 	   	die('Unable to connect to database [' . $mysqli->connect_error . ']');
 	}
 
+	$summonerIds = array(
+		'Biberman'	=> "40023404",
+	    'Lupusius'	=> "38527580",
+		'Lurchi09'	=> "40105796",
+		'Oglie'		=> "39999855",
+		'TomotX'	=> "41739529",
+	    'Viper6'	=> "38598275");
+
 	//gibt die matchIds aus, die noch nicht im table match sind
 	$query = '
 		SELECT DISTINCT ml.matchId
@@ -36,6 +44,7 @@
 		$json = file_get_contents($url);
 		$obj = json_decode($json, true);
 
+		//fügt generelle Matchdaten in table match ein
 		$query = '
 			INSERT INTO `match` (
 				matchId,
@@ -72,6 +81,93 @@
 		$mysqli->query($query);
 		unset($query);
 
+		//fügt Summoner spezifische Daten in table matchParticipants ein
+		$participants = array();
+		foreach ($obj['participantIdentities'] as $key => $value) {
+			if (in_array($value['player']['summonerId'], $summonerIds)) {
+				$participants[$key]['summonerId'] = $value['player']['summonerId'];
+				$participants[$key]['participantId'] = $value['participantId'];
+			}
+		}
+		foreach ($participants as $key0 => $participant) {
+
+			foreach ($obj['participants'] as $key => $value) {
+				if($value['participantId'] == $participant['participantId']){
+					$query = '
+						INSERT INTO matchParticipants (
+							matchId,
+							summonerId,
+							winner,
+							championId,
+							kills,
+							deaths,
+							assists,
+							minionsKilled,
+							champLevel,
+							goldEarned,
+							item0,
+							item1,
+							item2,
+							item3,
+							item4,
+							item5,
+							item6,
+							totalDamageDealtToChampions,
+							totalDamageTaken,
+							totalHeal,
+							totalTimeCrowdControlDealt
+						)
+						VALUES (
+							'.$matchIds[$i]['matchId'].',
+							'.$participant['summonerId'].',
+							'.($value['stats']['winner']>0?1:0).',
+							'.$value['championId'].',
+							'.$value['stats']['kills'].',
+							'.$value['stats']['deaths'].',
+							'.$value['stats']['assists'].',
+							'.$value['stats']['minionsKilled'].',
+							'.$value['stats']['champLevel'].',
+							'.$value['stats']['goldEarned'].',
+							'.$value['stats']['item0'].',
+							'.$value['stats']['item1'].',
+							'.$value['stats']['item2'].',
+							'.$value['stats']['item3'].',
+							'.$value['stats']['item4'].',
+							'.$value['stats']['item5'].',
+							'.$value['stats']['item6'].',
+							'.$value['stats']['totalDamageDealtToChampions'].',
+							'.$value['stats']['totalDamageTaken'].',
+							'.$value['stats']['totalHeal'].',
+							'.$value['stats']['totalTimeCrowdControlDealt'].'
+						)
+						ON DUPLICATE KEY UPDATE
+							winner 						=	VALUES(winner),
+							championId					= 	VALUES(championId),
+							kills						= 	VALUES(kills),
+							deaths 						= 	VALUES(deaths),
+							assists 					= 	VALUES(assists),
+							minionsKilled 				= 	VALUES(minionsKilled),
+							champLevel					= 	VALUES(champLevel),
+							goldEarned					=	VALUES(goldEarned),
+							item0 						= 	VALUES(item0),
+							item1 						= 	VALUES(item1),
+							item2 						= 	VALUES(item2),
+							item3 						= 	VALUES(item3),
+							item4 						= 	VALUES(item4),
+							item5 						= 	VALUES(item5),
+							item6 						= 	VALUES(item6),
+							totalDamageDealtToChampions	= 	VALUES(totalDamageDealtToChampions),
+							totalDamageTaken 			= 	VALUES(totalDamageTaken),
+							totalHeal 					=	VALUES(totalHeal),
+							totalTimeCrowdControlDealt	= 	VALUES(totalTimeCrowdControlDealt)
+					';
+					$mysqli->query($query);
+					unset($query);
+				}
+			}
+		}
+		unset($participants);
+		unset($url,$json,$obj);
 	}
 	unset($matchIds);
 
