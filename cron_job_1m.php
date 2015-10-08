789b71b5b828676d2ea5a425c55a129b64004361
viper6689 <a349581@drdrb.net>
2015-10-08 19:51:57 +0200

    Update cron_job_1m.php

--- cron_job_1m.php
+++ cron_job_1m.php
@@ -1,12 +1,14 @@
 <?php
 	/* --- CRON JOB (every 1 minute) --- */
 
-	//MySQLi connection
+	//--- MySQLi connection ---
 	$mysqli = new mysqli('localhost', 'rclsirzj_lolmeta', 'LoLMeta42', 'rclsirzj_matchHistory');
 	if($mysqli->connect_errno > 0){
 	   	die('Unable to connect to database [' . $mysqli->connect_error . ']');
 	}
 
+
+    //--- Variables ---
 	//globaleVariable
     $result = $mysqli->query('SELECT globeVar FROM dbVar LIMIT 1');
 	$globaleVar = $result->fetch_assoc();
@@ -14,10 +16,10 @@
 
     switch ($globaleVar['globeVar']) {
     	case '1':
-    		$cronUrl = "http://lolmeta.serverlux.me/matchlist.php";
+    		$cronUrl = 'http://lolmeta.serverlux.me/getJson_matchlist.php';
     		break;
     	case '2':
-    		$cronUrl = "http://lolmeta.serverlux.me/match.php";
+    		$cronUrl = 'http://lolmeta.serverlux.me/getJson_match.php';
     		break;
 
     	default:
@@ -25,14 +27,8 @@
     		break;
     }
 
-    function curl($url) {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-        $data = curl_exec($ch);
-        curl_close($ch);
-        return $data;
-    }
+    
+    //--- Methodes ---
     echo curl($cronUrl);
 
     if ($globaleVar['globeVar'] > 1) {
@@ -40,7 +36,19 @@
     } else {
     	$mysqli->query('UPDATE dbVar SET globeVar = globeVar+1');	
     }
+
     
-    //MySQLi disconnection
+    //--- Functions ---
+    function curl($url) {
+        $ch = curl_init();
+        curl_setopt($ch,CURLOPT_URL,$url);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return $data;
+    }
+
+
+    //--- MySQLi disconnection ---
 	$mysqli->close();
 ?>
