f48971c246c691372105c2d0da8266eb760cc669
viper6689 <a349581@drdrb.net>
2015-10-09 18:26:15 +0200

    Update matchHistory.sql

--- matchHistory.sql
+++ matchHistory.sql
@@ -3,8 +3,8 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 05, 2015 at 05:14 PM
--- Server version: 5.5.42-cll
+-- Generation Time: Oct 09, 2015 at 06:25 PM
+-- Server version: 5.5.45-cll
 -- PHP Version: 5.4.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -13,6 +13,16 @@ SET time_zone = "+00:00";
 --
 -- Database: `rclsirzj_matchHistory`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dbVar`
+--
+
+CREATE TABLE IF NOT EXISTS `dbVar` (
+  `globeVar` tinyint(2) unsigned NOT NULL DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -48,6 +58,25 @@ CREATE TABLE IF NOT EXISTS `league` (
 --
 
 CREATE TABLE IF NOT EXISTS `match` (
+  `matchId` int(10) unsigned NOT NULL DEFAULT '0',
+  `queueType` set('RANKED_SOLO_5x5','RANKED_TEAM_3x3','RANKED_TEAM_5x5') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'RANKED_SOLO_5x5',
+  `matchCreation` bigint(13) unsigned NOT NULL DEFAULT '0',
+  `matchDuration` smallint(4) unsigned NOT NULL DEFAULT '0',
+  `mapId` tinyint(2) unsigned NOT NULL DEFAULT '0',
+  `matchMode` set('CLASSIC','ODIN','ARAM','TUTORIAL','ONEFORALL','ASCENSION','FIRSTBLOOD','KINGPORO') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'CLASSIC',
+  `matchType` set('CUSTOM_GAME','MATCHED_GAME','TUTORIAL_GAME') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'MATCHED_GAME',
+  `matchVersion` varchar(11) COLLATE utf8_unicode_ci NOT NULL DEFAULT '00.00.0.000',
+  `season` set('PRESEASON2015','SEASON2015','PRESEASON2016','SEASON2016') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'PRESEASON2015',
+  UNIQUE KEY `index` (`matchId`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matches`
+--
+
+CREATE TABLE IF NOT EXISTS `matches` (
   `matchId` int(10) unsigned NOT NULL DEFAULT '0',
   `queueType` set('RANKED_SOLO_5x5','RANKED_TEAM_3x3','RANKED_TEAM_5x5') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'RANKED_SOLO_5x5',
   `matchCreation` bigint(13) unsigned NOT NULL DEFAULT '0',
@@ -107,4 +136,57 @@ CREATE TABLE IF NOT EXISTS `matchParticipants` (
   `totalHeal` mediumint(6) unsigned NOT NULL DEFAULT '0',
   `totalTimeCrowdControlDealt` smallint(4) unsigned NOT NULL DEFAULT '0',
   UNIQUE KEY `index` (`matchId`,`summonerId`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `summonerMatchstats`
+--
+
+CREATE TABLE IF NOT EXISTS `summonerMatchstats` (
+  `matchId` int(10) unsigned DEFAULT NULL,
+  `summonerId` int(8) unsigned DEFAULT NULL,
+  `championId` smallint(3) unsigned NOT NULL DEFAULT '0',
+  `lane` set('MID','MIDDLE','TOP','JUNGLE','BOT','BOTTOM') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'TOP',
+  `role` set('DUO','NONE','SOLO','DUO_CARRY','DUO_SUPPORT') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'NONE',
+  `win` tinyint(1) NOT NULL DEFAULT '0',
+  `kills` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `deaths` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `assists` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `minionsKilled` smallint(4) unsigned NOT NULL DEFAULT '0',
+  `champLevel` tinyint(2) unsigned NOT NULL DEFAULT '0',
+  `goldEarned` mediumint(6) unsigned NOT NULL DEFAULT '0',
+  `item0` smallint(4) unsigned NOT NULL DEFAULT '0',
+  `item1` smallint(4) unsigned NOT NULL DEFAULT '0',
+  `item2` smallint(4) unsigned NOT NULL DEFAULT '0',
+  `item3` smallint(4) unsigned NOT NULL DEFAULT '0',
+  `item4` smallint(4) unsigned NOT NULL DEFAULT '0',
+  `item5` smallint(4) unsigned NOT NULL DEFAULT '0',
+  `item6` smallint(4) unsigned NOT NULL DEFAULT '0',
+  `totalDamageDealtToChampions` mediumint(6) unsigned NOT NULL DEFAULT '0',
+  `totalDamageTaken` mediumint(6) unsigned NOT NULL DEFAULT '0',
+  `totalHeal` mediumint(6) unsigned NOT NULL DEFAULT '0',
+  `totalTimeCrowdControlDealt` smallint(4) unsigned NOT NULL DEFAULT '0',
+  `complete` tinyint(1) NOT NULL DEFAULT '0',
+  UNIQUE KEY `index` (`matchId`,`summonerId`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teamHistory`
+--
+
+CREATE TABLE IF NOT EXISTS `teamHistory` (
+  `matchId` int(10) unsigned DEFAULT NULL,
+  `teamId` varchar(41) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `win` tinyint(1) NOT NULL DEFAULT '0',
+  `kills` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `deaths` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `assists` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `opposingTeamName` varchar(30) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'unknown',
+  `opposingTeamKills` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `invalid` tinyint(1) NOT NULL DEFAULT '0',
+  UNIQUE KEY `matchId` (`matchId`,`teamId`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
